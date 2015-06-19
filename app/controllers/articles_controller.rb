1d020db7341809ab91c39b0627537c294b40b92e
Athif Wulandana <athifwulandana-mac@Athifs-MacBook-Pro.local>
2015-06-19 16:58:51 -0400

    Added new article page, tests

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,25 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.order(created_at: :desc)
   end
 
-   
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end 
+
+  private
+
+  def article_params
+    params[:article].permit(:title, :url, :author)
+  end
 end
--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -4,11 +4,35 @@ RSpec.describe "Managing articles" do
   describe "viewing the index page" do
     let!(:post_1) { create :article }
     let!(:post_2) { create :article }
-    
+
     it "shows all articles" do
       visit '/articles'
       expect(page).to have_content(post_1.title)
       expect(page).to have_content(post_2.title)
+    end
+  end
+  
+  describe "creating an article" do
+    before do
+      visit '/articles'
+      click_link "New Article"
+    end
+
+    describe "missing some fields" do
+      it "shows errors" do
+        click_button "Create Article"
+        expect(page).to have_content("Title can't be blank")
+      end
+    end
+
+    describe "all fields present" do
+      it "displays article" do
+        fill_in 'Title', with: 'Created Title'
+        fill_in 'URL', with: 'http://github.com'
+        fill_in 'Author', with: 'Me'
+        click_button "Create Article"
+        expect(page).to have_content("Created Title")
+      end
     end
   end
 end
