ed2d62aa680996ecea8e89bf0dec2d7985b41085
Athif Wulandana <athifwulandana-mac@Athifs-MacBook-Pro.local>
2015-06-23 16:47:36 -0400

    Some user stuff

--- db/migrate/20150623164350_add_null_constraint_to_article_fields.rb
+++ db/migrate/20150623164350_add_null_constraint_to_article_fields.rb
@@ -2,6 +2,6 @@ class AddNullConstraintToArticleFields < ActiveRecord::Migration
   def change
     change_column_null :articles, :title, false
     change_column_null :articles, :url, false
-    change_column_null :articles, :author, false
+    change_column_null :articles, :password_digest, false
   end
 end
