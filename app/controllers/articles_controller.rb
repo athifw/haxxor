0c16358513c21cba424d8bf42ecadb8d8d7f7ac2
Athif Wulandana <athifwulandana-mac@Athifs-MacBook-Pro.local>
2015-06-23 13:12:31 -0400

    Limited article view on index to 20 articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.order(created_at: :desc)
+    @articles = Article.order(created_at: :desc).limit(20)
   end
 
   def new
